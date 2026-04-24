59179ad4d4bb01b5b6a44f71b75ba0fdf1e99309
Sudhanshu Singh <159468932+ssr3112@users.noreply.github.com>
2026-04-25 00:58:01 +0530

    Add files via upload

---
+++ app_core/embedings/embedding_model.py
@@ -0,0 +1,7 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed_texts(texts):
+    embeddings = model.encode(texts, convert_to_numpy=True)
+    return embeddings
